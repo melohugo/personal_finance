6eef376d07575c31b7ae61cb2efae692bb0332c5
melohugo <hugoq2004@gmail.com>
2026-03-23 14:05:01 -0300

    feat: inicializa projeto nestjs com arquitetura modular, prisma e docker

---
+++ src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ExpensesModule } from './modules/expenses/expenses.module';
+import { InvestmentsModule } from './modules/investments/investments.module';
+import { MarketModule } from './modules/market/market.module';
+import { ProcessorModule } from './modules/processor/processor.module';
+import { TelegramModule } from './modules/telegram/telegram.module';
+
+@Module({
+  imports: [ExpensesModule, InvestmentsModule, MarketModule, ProcessorModule, TelegramModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
